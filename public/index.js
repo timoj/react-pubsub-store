d35b68932a87308ea5e4a200a474101f0af13a70
Timo Janssrn <tj.timo.j@gmail.com>
2018-05-24 16:32:06 +0200

    fix transpiling

--- public/index.js
+++ public/index.js
@@ -1,44 +1,53 @@
-"use strict";
+(function (global) {
+    "use strict";
+    let ReactPubSubStore = (function (f) {
+        let fetcher = f;
+        let topics = {};
+        let topicsData = {};
+        let hOP = topics.hasOwnProperty;
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
 
-var ReactPubSubStore = function (f) {
-    var fetcher = f;
-    var topics = {};
-    var topicsData = {};
-    var hOP = topics.hasOwnProperty;
+        let updateTopicData = (topic) => {
+            fetcher.fetchResource(topic, (response) => {
+                topicsData[topic] = response;
 
-    var updateTopicData = function updateTopicData(topic) {
-        fetcher.fetchResource(topic, function (response) {
-            topicsData[topic] = response;
-
-            topics[topic].forEach(function (item) {
-                item(response !== undefined ? response : {});
+                topics[topic].forEach(function (item) {
+                    item(response !== undefined ? response : {});
+                });
             });
-        });
-    };
+        };
 
-    return {
-        subscribe: function subscribe(topic, listener) {
-            // Create the topic's object if not yet created
-            if (!hOP.call(topics, topic)) {
-                topics[topic] = [];
-                updateTopicData(topic);
-            }
-
-            // Add the listener to queue
-            var index = topics[topic].push(listener) - 1;
-
-            // Provide handle back for removal of topic
-            return {
-                remove: function remove() {
-                    delete topics[topic][index];
+        return {
+            subscribe: function (topic, listener) {
+                // Create the topic's object if not yet created
+                if (!hOP.call(topics, topic)) {
+                    topics[topic] = [];
+                    updateTopicData(topic);
                 }
-            };
-        }
-    };
-}();
 
-exports.default = ReactPubSubStore;
+                // Add the listener to queue
+                let index = topics[topic].push(listener) - 1;
+
+                // Provide handle back for removal of topic
+                return {
+                    remove: function () {
+                        delete topics[topic][index];
+                    }
+                };
+            }
+        };
+    })();
+    if (typeof module === 'object' && module && typeof module.exports === 'object') {
+        module.exports = ReactPubSubStore;
+
+        // Register as an AMD module
+    } else if (typeof define === 'function' && define.amd) {
+        define('ReactPubSubStore', [], function () {
+            return ReactPubSubStore;
+        });
+
+        // Export into global space
+    } else if (typeof global === 'object' && typeof global.document === 'object') {
+        global.ReactPubSubStore = ReactPubSubStore;
+    }
+})(this);
--- src/index.js
+++ src/index.js
@@ -1,40 +1,53 @@
+(function (global) {
+    "use strict";
+    let ReactPubSubStore = (function (f) {
+        let fetcher = f;
+        let topics = {};
+        let topicsData = {};
+        let hOP = topics.hasOwnProperty;
 
-let ReactPubSubStore = (function (f) {
-    let fetcher = f;
-    let topics = {};
-    let topicsData = {};
-    let hOP = topics.hasOwnProperty;
 
+        let updateTopicData = (topic) => {
+            fetcher.fetchResource(topic, (response) => {
+                topicsData[topic] = response;
 
-    let updateTopicData = (topic) => {
-        fetcher.fetchResource(topic, (response) => {
-            topicsData[topic] = response;
-
-            topics[topic].forEach(function (item) {
-                item(response !== undefined ? response : {});
+                topics[topic].forEach(function (item) {
+                    item(response !== undefined ? response : {});
+                });
             });
-        });
-    };
+        };
 
-    return {
-        subscribe: function (topic, listener) {
-            // Create the topic's object if not yet created
-            if (!hOP.call(topics, topic)) {
-                topics[topic] = [];
-                updateTopicData(topic);
-            }
-
-            // Add the listener to queue
-            let index = topics[topic].push(listener) - 1;
-
-            // Provide handle back for removal of topic
-            return {
-                remove: function () {
-                    delete topics[topic][index];
+        return {
+            subscribe: function (topic, listener) {
+                // Create the topic's object if not yet created
+                if (!hOP.call(topics, topic)) {
+                    topics[topic] = [];
+                    updateTopicData(topic);
                 }
-            };
-        }
-    };
-})();
 
-export default ReactPubSubStore;
+                // Add the listener to queue
+                let index = topics[topic].push(listener) - 1;
+
+                // Provide handle back for removal of topic
+                return {
+                    remove: function () {
+                        delete topics[topic][index];
+                    }
+                };
+            }
+        };
+    })();
+    if (typeof module === 'object' && module && typeof module.exports === 'object') {
+        module.exports = ReactPubSubStore;
+
+        // Register as an AMD module
+    } else if (typeof define === 'function' && define.amd) {
+        define('ReactPubSubStore', [], function () {
+            return ReactPubSubStore;
+        });
+
+        // Export into global space
+    } else if (typeof global === 'object' && typeof global.document === 'object') {
+        global.ReactPubSubStore = ReactPubSubStore;
+    }
+})(this);
