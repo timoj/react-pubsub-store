c6a921f8e711b5419262c00983bec6c39bc8f91c
Timo Janssrn <tj.timo.j@gmail.com>
2018-08-01 17:13:54 +0200

    bugfix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
 
     ReactPubSubStore._updateTopicData = function (topic, append) {
         let url = topic;
-        if ((ReactPubSubStore._topicsOptions[topic] !== undefined && ReactPubSubStore._topicsOptions[topic].urlBuilder !== undefined && typeof ReactPubSubStore._topicsOptions[topic].urlBuilder === "function") {
+        if (ReactPubSubStore._topicsOptions[topic] !== undefined && ReactPubSubStore._topicsOptions[topic].urlBuilder !== undefined && typeof ReactPubSubStore._topicsOptions[topic].urlBuilder === "function") {
             url = ReactPubSubStore._topicsOptions[topic].urlBuilder(ReactPubSubStore._topicsOptions[topic]);
         }
         ReactPubSubStore._dao.fetchResource(url, (response) => {
